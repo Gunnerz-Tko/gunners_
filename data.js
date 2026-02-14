7f8cd49007f76d89dab7281af84a9f7fbb8a2d05
Gunnerz-Tko <carrere.cyril@gmail.com>
2026-02-15 00:50:10 +0900

    Add data.js file with oricon_data object containing test book data for 5 genres

---
+++ data.js
@@ -0,0 +1,24 @@
+const oricon_data = {
+    pop: [
+        { title: "Pop Book 1", author: "Author A", rating: 4.5 },
+        { title: "Pop Book 2", author: "Author B", rating: 4.0 }
+    ],
+    rock: [
+        { title: "Rock Book 1", author: "Author C", rating: 4.7 },
+        { title: "Rock Book 2", author: "Author D", rating: 3.8 }
+    ],
+    jazz: [
+        { title: "Jazz Book 1", author: "Author E", rating: 5.0 },
+        { title: "Jazz Book 2", author: "Author F", rating: 4.9 }
+    ],
+    classical: [
+        { title: "Classical Book 1", author: "Author G", rating: 4.3 },
+        { title: "Classical Book 2", author: "Author H", rating: 4.6 }
+    ],
+    hiphop: [
+        { title: "HipHop Book 1", author: "Author I", rating: 4.1 },
+        { title: "HipHop Book 2", author: "Author J", rating: 4.2 }
+    ]
+};
+
+export default oricon_data;
